4885e5af03baece077af238f7d4161e3be2b2942
Sophia Barrett <sophia@yoursummit.media>
2022-02-06 13:30:18 -0700

    initial LoggedTime model setup

--- server/models/LoggedTime.js
+++ server/models/LoggedTime.js
@@ -1,1 +1,19 @@
-//LoggedTime Model
+const { Schema } = require("mongoose");
+
+const loggedTimeSchema = new Schema({
+  description: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+    required: true,
+  },
+  hours: {
+    type: Number,
+    required: true,
+  },
+});
+
+module.exports = loggedTimeSchema;
