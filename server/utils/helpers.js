92fe97633824f14afc37b8923e8a2d74444382c5
Sophia Barrett <sophia@yoursummit.media>
2022-02-06 14:38:39 -0700

    add comments to helpers

--- server/utils/helpers.js
+++ server/utils/helpers.js
@@ -1,3 +1,4 @@
+// format just the date (E.g., Feb 6, 2022)
 export function formatDate(date) {
     return new Date(date).toLocaleString("en-us", {
         month: "short",
@@ -5,6 +6,8 @@ export function formatDate(date) {
         year: "numeric",
     });
 }
+
+// format datetime (E.g., 4:37 pm on Feb 6, 2022)
 export function formatDatetime(datetime) {
     const date = new Date(datetime).toLocaleString("en-us", {
         month: "short",
@@ -16,6 +19,8 @@ export function formatDatetime(datetime) {
         .toLowerCase();
     return `${time} on ${date}`;
 }
+
+// format hour count to 2 decimal places (E.g., 1 => 1.00)
 export function formatHours(hours) {
     return parseFloat(hours).toFixed(2);
 }
