ed3dbffc0077146e9f16a12c79fb3e62b849c7f2
Hailey Thomas <haileyraebeauty@gmail.com>
2022-02-07 13:48:28 -0700

    create Task model

--- server/models/Task.js
+++ server/models/Task.js
@@ -1,1 +1,41 @@
-//Task Model
+import { Schema, model } from "mongoose";
+
+const TaskSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: [true, "Title is required."],
+    },
+    description: {
+      type: String,
+      required: [true, "Description is required."],
+    },
+    status: {
+      type: String,
+      enum: ["Requested", "In Progress", "Completed"],
+      default: "Requested",
+      required: [true, "Status is required."],
+    },
+    estimatedHours: {
+      type: Number,
+      required: [true, "Estimated hours is required."],
+    },
+    timeLog: [{ type: Schema.Types.ObjectId, ref: "LoggedTime" }],
+    comments: [{ type: Schema.Types.ObjectId, ref: "Comment" }],
+  },
+  {
+    toJSON: {
+      getters: true,
+      virtuals: true,
+    },
+  }
+);
+
+// virtual for logging totalTime
+TaskSchema.virtual("totalTime").get(function () {
+  return this.timeLog.reduce((a, b) => a + b, 0);
+});
+
+const Task = model("Task", TaskSchema);
+
+export default Task;
