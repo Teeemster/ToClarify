cc9b0c6060da716d127e6188023d5b3368230ba8
arios10 <ariosfly3@gmail.com>
2022-02-08 14:14:09 -0700

    created Project model

--- server/models/Project.js
+++ server/models/Project.js
@@ -1,1 +1,36 @@
 //Project Model
+const { Schema, model } = require('mongoose');
+
+
+const projectSchema = new Schema(
+    {
+        Title: {
+            type: String,
+            required: 'Project must have a title!',
+            minlength: 1,
+            maxlength: 80
+        },
+        Owner: {
+            type: String,
+            enum: ['User', 'Admin'],
+            default: 'User',
+            required: 'Owner is required!'
+        },
+        Tasks: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Task'
+            }
+        ],
+        Clients: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'User'
+            }
+        ]
+    },
+);
+
+const Project = model('Project', ProjectSchema);
+
+export default Project;
