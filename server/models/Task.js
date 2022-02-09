785203dc1c8ed7fa0cef27464ff9b685a407ebae
Hailey Thomas <haileyraebeauty@gmail.com>
2022-02-08 18:50:30 -0700

    change task capitalize

--- server/models/Task.js
+++ server/models/Task.js
@@ -1,7 +1,7 @@
 import { Schema, model } from "mongoose";
 import { formatHours } from "../utils/helpers";
 
-const TaskSchema = new Schema(
+const taskSchema = new Schema(
   {
     title: {
       type: String,
