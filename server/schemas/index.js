7ad47e7d0a2e1f664996cc2350261a274e2d5163
Sophia Barrett <sophia@yoursummit.media>
2022-02-13 11:52:44 -0700

    update model export sytax and setup resolvers.js

--- server/schemas/index.js
+++ server/schemas/index.js
@@ -1,4 +1,4 @@
 import typeDefs from "./typeDefs";
 import resolvers from "./resolvers";
 
-module.export = { resolvers };
+export default { typeDefs, resolvers };
---
+++ server/schemas/resolvers.js
@@ -0,0 +1,8 @@
+import { User, Project, Task, Comment, LoggedTime } from "../models";
+
+const resolvers = {
+  Query: {},
+  Mutation: {},
+};
+
+export default resolvers;
