fabcd44af263657e32b451f631ddbb488d863a45
Teeemster <jonathan_beach@ymail.com>
2022-02-17 21:40:08 -0700

    added forms and lists

--- client/src/components/ProjectList/index.js
+++ client/src/components/ProjectList/index.js
@@ -1,1 +1,44 @@
-//List of all the projects
+//List of all the projects for an individual
+//Project List Component
+import React, { useState } from 'react';
+
+import { QUERY_ME } from '../../utils/queries';
+import { useMutation } from '@apollo/client';
+import { ADD_PROJECT } from '../../utils/mutations';
+
+const ProjectList = () => {
+    const [projectText, setProjectText] = useState('');
+    
+    //Update Project Cache Array
+    const [addProject, { error }] = useMutation(ADD_PROJECT, {
+        update(cache, { data: { addProject } }) {
+            try {
+                const { project } = cache.readQuery({ query: QUERY_ME });
+                cache.writeQuery({
+                    query: QUERY_ME,
+                    data: { project: [addProject, ...project] },
+                });
+            } catch (e) {
+                console.error(e);
+            }
+            //Update Me Object Cache
+            const { me } = cache.readQuery({ query: QUERY_ME });
+            cache.writeQuery({
+                query: QUERY_ME,
+                data: { me: { ...me, project: [...me.task, addProject] } },
+            });
+        },
+    });
+
+
+    return (
+        <section>
+            <div>
+
+            </div>
+            <button>📍 Add New Project</button>
+        </section>
+    );
+};
+
+export default ProjectList;
