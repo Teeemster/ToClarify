c9f358d3f7680854eee7f13e612f39c2e4937997
Teeemster <jonathan_beach@ymail.com>
2022-02-17 18:00:40 -0700

    added login and signup form with state

--- client/src/components/LoginForm/index.js
+++ client/src/components/LoginForm/index.js
@@ -1,15 +1,55 @@
 //Login Component
-import React from 'react';
+import React, { useState } from 'react';
+
+import { validateEmail } from '../../utils/helpers';
 
 const LoginForm = () => {
+    const [formState, setFormState] = useState({ email: '', password: '' });
+
+    const [errorMessage, setErrorMessage] = useState('');
+    const { email, password } = formState;
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (!errorMessage) {
+            console.log('Submit Form', formState);
+        }
+    };
+
+    const handleChange = (e) => {
+        if (e.target.name === 'email') {
+            const isValid = validateEmail(e.target.value);
+            if (!isValid) {
+                setErrorMessage('This is not a valid email.');
+            } else {
+                setErrorMessage('');
+            }
+        } else {
+            if (!e.target.value.length) {
+                setErrorMessage(`${e.target.name} is required.`);
+            } else {
+                setErrorMessage('');
+            }
+        }
+        if (!errorMessage) {
+            setFormState({ ...formState, [e.target.name]: e.target.value });
+            console.log('Handle Form', formState);
+        }
+    };
+
+
     return (
-        <div>
-            <form>
+        <section>
+            <h1>Sign-In!</h1>
+            <form onSubmit={handleSubmit}>
+
                 <input
                     placeholder="Your email"
                     name="email"
                     type="email"
                     id="email"
+                    defaultValue={email}
+                    onBlur={handleChange}
                 ></input>
 
                 <input
@@ -17,10 +57,13 @@ const LoginForm = () => {
                     name="password"
                     type="password"
                     id="password"
+                    defaultValue={password}
+                    onBlur={handleChange}
                 >
                 </input>
+                <button>Login</button>
             </form>
-        </div>
+        </section>
     );
 };
 
