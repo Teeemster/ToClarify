fd08eda83afdcf425c412e411c2ac459b1121185
Hailey Thomas <haileyraebeauty@gmail.com>
2022-02-18 13:02:27 -0700

    add QUERY_ME to welcome page

--- client/src/components/Welcome/index.js
+++ client/src/components/Welcome/index.js
@@ -1,7 +1,21 @@
 import React from "react";
+import { useQuery } from "@apollo/client";
+import { QUERY_ME } from "../../utils/queries";
+import logo from "../../../public/logo512.png";
 
-const Welcome = () => {
-  return <div></div>;
-};
+function Welcome() {
+    const { loading, data } = useQuery(QUERY_ME);
+    const me = data?.me || {};
+
+    if (loading) {
+        return <div>Loading...</div>;
+    }
+
+    return (
+        <div>
+            <h3>Welcome Back To Clarity {me.firstName}</h3>
+        </div>
+    );
+}
 
 export default Welcome;
