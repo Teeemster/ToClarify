c6f4deeda3418f35c5738c545ed501a98e487899
Hailey Thomas <haileyraebeauty@gmail.com>
2022-02-18 13:09:57 -0700

    add welcome message

--- client/src/components/Welcome/index.js
+++ client/src/components/Welcome/index.js
@@ -4,18 +4,22 @@ import { QUERY_ME } from "../../utils/queries";
 import logo from "../../../public/logo512.png";
 
 function Welcome() {
-    const { loading, data } = useQuery(QUERY_ME);
-    const me = data?.me || {};
+  const { loading, data } = useQuery(QUERY_ME);
+  const me = data?.me || {};
 
-    if (loading) {
-        return <div>Loading...</div>;
-    }
+  if (loading) {
+    return <div>Loading...</div>;
+  }
 
-    return (
-        <div>
-            <h3>Welcome Back To Clarity {me.firstName}</h3>
-        </div>
-    );
+  return (
+    <div>
+      <img src={logo} alt="Magnify glass logo" />
+      <h2>Welcome Back To Clarity {me.firstName}</h2>
+      <p>
+        Select one of your projects or click "Add New Project" to get started!
+      </p>
+    </div>
+  );
 }
 
 export default Welcome;
