cfbe8a7031c92466dfad8c656f9f3ceea0af67ee
Hailey Thomas <haileyraebeauty@gmail.com>
2022-02-18 10:29:11 -0700

    add me query to header

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,14 +1,24 @@
 //Header Component
 import React from "react";
 import { Link } from "react-router-dom";
+import { useQuery } from "@apollo/client";
+import { QUERY_ME } from "../../utils/queries";
 
 // TODO update header so User renders the user's firstName
 // TODO add home icon?
 const Header = () => {
+  const { loading, data } = useQuery(QUERY_ME);
+
+  const me = data?.me || {};
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <div>
       <div>
-        <h1>Welcome To Clarity, User.</h1>
+        <h1>Welcome To Clarity, {me.firstName}.</h1>
       </div>
       <nav>
         <Link to="/signup" className="">
