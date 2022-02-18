96d76994cca1d7dd9761e323bea7a00079d025fe
Hailey Thomas <haileyraebeauty@gmail.com>
2022-02-17 19:53:53 -0700

    create basic TaskDetail

--- client/src/components/TaskDetail/index.js
+++ client/src/components/TaskDetail/index.js
@@ -1,1 +1,20 @@
 //Task detail
+import React from "react";
+import CommentList from "../CommentList";
+import TimeLog from "../TimeLog";
+
+const TaskDetail = () => {
+  return (
+    <div>
+      <div>
+        <div></div>
+        <CommentList />
+      </div>
+      <div>
+        <TimeLog />
+      </div>
+    </div>
+  );
+};
+
+export default TaskDetail;
