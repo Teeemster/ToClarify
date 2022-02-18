769fc78a6a233e3abe850251fb39f3481499799f
Hailey Thomas <haileyraebeauty@gmail.com>
2022-02-17 17:48:06 -0700

    add footer html

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -1,12 +1,26 @@
 //Footer Component
-import React from 'react';
+import React from "react";
 
 const Footer = () => {
-    return (
-        <footer>
-            📌 By Hailey Thomas, Sophie Barrett, Angel Rios, and Jonathan Beach 📌
-        </footer>
-    );
+  let currentYear = new Date().getFullYear();
+  return (
+    <div>
+      <p>
+        &copy; {currentYear} By Hailey Thomas, Sophie Barrett, Angel Rios, and
+        Jonathan Beach
+        <span role="img" aria-label="thumbtack">
+          📌
+        </span>
+      </p>
+      <a
+        href="https://github.com/Teeemster/Project3"
+        target="_blank"
+        rel="noreferrer"
+      >
+        View Project on GitHub
+      </a>
+    </div>
+  );
 };
 
 export default Footer;
