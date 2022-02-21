bd8cc85d796385f69ae10792ee9534134332723a
Sophia Barrett <sophia@yoursummit.media>
2022-02-20 21:25:54 -0700

    add responsive styles to home page

--- client/src/components/ProjectList/index.js
+++ client/src/components/ProjectList/index.js
@@ -15,20 +15,20 @@ const ProjectList = () => {
   return (
     <div className="m-3">
       <h3 className="fs-4 fw-bold">My Projects</h3>
-      {!projects.length ? (
-        <p className="mt-3">You haven't started any projects, yet.</p>
-      ) : (
-        <ul className="list-group list-group-flush">
-          {projects.map((project) => (
-            <li key={project._id} className="list-group-item bg-grey">
-              <Link to={`/project/${project._id}`}>{project.title}</Link>
-            </li>
-          ))}
-        </ul>
-      )}
-      <div className="mt-3">
-        <ProjectForm />
-      </div>
+        {!projects.length ? (
+          <p className="mt-3">You haven't started any projects, yet.</p>
+        ) : (
+          <ul className="list-group list-group-flush">
+            {projects.map((project) => (
+              <li key={project._id} className="list-group-item bg-grey">
+                <Link to={`/project/${project._id}`}>{project.title}</Link>
+              </li>
+            ))}
+          </ul>
+        )}
+        <div className="mt-3">
+          <ProjectForm />
+        </div>
     </div>
   );
 };
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,6 +1,6 @@
 //Home Page
 import React from "react";
-import ProjectArea from "../components/ProjectArea";
+import Welcome from "../components/Welcome";
 import ProjectList from "../components/ProjectList";
 import { Navigate } from "react-router-dom";
 import Auth from "../utils/auth";
@@ -19,33 +19,13 @@ const Home = () => {
 
   return (
     <div className="d-flex w-100 m-0 row">
-      <div className="col-3 p-0 bg-grey">
+      <div className="col-md-3 p-0 bg-grey">
         <ProjectList />
       </div>
-      <div className="col p-0">
-        {/* <ProjectArea /> */}
+      <div className="d-none d-md-block col-md-9 p-0">
+        <Welcome />
       </div>
     </div>
-
-    // <main>
-    //   <div className="flex-row justify-space-between">
-    //     {loggedIn && (
-    //       <div className="col-12 mb-3">
-    //         <ProjectForm />
-    //       </div>
-    //     )}
-    //     <div className={`col-12 mb-3 ${loggedIn && 'col-lg-8'}`}>
-    //       {loading ? (
-    //         <div>Loading...</div>
-    //       ) : (
-    //         <ProjectList
-    //           project={project}
-    //           title="Your Projects"
-    //         />
-    //       )}
-    //     </div>
-    //   </div>
-    // </main>
   );
 };
 
