a813c1fa7b93d0978d60d592bb964a3cf873c1c8
Sophia Barrett <sophia@yoursummit.media>
2022-03-19 19:45:21 -0700

    update project settings page to display message to non-admin

--- client/src/components/ProjectSettings/index.js
+++ client/src/components/ProjectSettings/index.js
@@ -3,7 +3,7 @@ import React from "react";
 import { useParams, Link } from "react-router-dom";
 
 import { useQuery } from "@apollo/client";
-import { QUERY_PROJECT } from "../../utils/queries";
+import { QUERY_PROJECT, QUERY_ME } from "../../utils/queries";
 
 import Icon from "@mdi/react";
 import { mdiMenu } from "@mdi/js";
@@ -12,16 +12,55 @@ import AddClientForm from "../AddClientForm";
 const ProjectSettings = () => {
   const { projectId } = useParams();
 
-  const { loading, data } = useQuery(QUERY_PROJECT, {
-    variables: { id: projectId },
-  });
+  const { loading: projectLoading, data: projectData } = useQuery(
+    QUERY_PROJECT,
+    {
+      variables: { id: projectId },
+    }
+  );
+  const { loading: userLoading, data: userData } = useQuery(QUERY_ME);
 
-  const project = data?.project || {};
+  const project = projectData?.project || {};
+  const me = userData?.me || {};
 
-  if (loading) {
+  if (projectLoading || userLoading) {
     return (
       <div className="m-4">
         <h2>Loading...</h2>
+      </div>
+    );
+  }
+
+  // After project data finishes loading, check if current user is a project owner
+  let adminPermissions = project.owners.some((owner) => owner._id === me._id);
+
+  if (!adminPermissions) {
+    return (
+      <div className="m-4">
+        <div className="clearfix mb-4">
+          <div className="float-start">
+            <h2 className="fw-bold">
+              <Link to="/" className="d-inline d-md-none link-white">
+                <Icon path={mdiMenu} size={1.5} />
+              </Link>{" "}
+              <Link to={`/project/${project._id}`} className="link-white">
+                {project.title}
+              </Link>
+              {" > "}
+              Settings
+            </h2>
+          </div>
+          <div className="float-end">
+            <Link to={`/project/${projectId}`}>
+              <button className="btn btn-purple text-white fw-bold d-none d-md-inline-block">
+                {"< "}Back to Project
+              </button>
+            </Link>
+          </div>
+        </div>
+        <div>
+          <p className="fs-5">Sorry, you don't have permission to updating settings for this project.</p>
+        </div>
       </div>
     );
   }
